d178fc2ef64ac43f372d7cf41228005010524360
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-07-15 15:06:54 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MongoDBCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MongoClient("mongodb+srv://tomateDataBase:tomatemasqueunataqueria@cluster0.ficwm8y.mongodb.net/?retryWrites=true&w=majority");
+            var database = client.GetDatabase("test");
+            var collection = database.GetCollection<BsonDocument>("users");
+
+            // Consultar todos los documentos en la colección "users"
+            var documents = collection.Find(new BsonDocument()).ToList();
+
+            // Mostrar los documentos por consola
+            Console.WriteLine("Documentos en la colección 'users':");
+            foreach (var doc in documents)
+            {
+                Console.WriteLine(doc.ToJson());
+            }
+        }
+    }
+}
